ec411f04a5e54d2560505d690fcb17ede0ea6429
mcdupas <dupas.mc@gmail.com>
2024-06-17 15:56:34 +0200

    feat(fdm7.py): adapt the code to predict stock of farms simulated with LGCP, with different level clustering (varying "variance" and "scale" parameters)

--- 02_Codes/fdm_7_predict_farm_size.py
+++ 02_Codes/fdm_7_predict_farm_size.py
@@ -1,5 +1,4 @@
 """Module providing a function printing python version."""
-
 import os
 import pickle
 import pandas as pd
@@ -8,9 +7,10 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 from scipy.stats import gaussian_kde
+import itertools
 
 ###### sklearn (machine learning package)
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import StandardScaler
@@ -59,11 +59,14 @@ ensure_dir(OUTPUT_FOLDER)
 BUFFER_DISTANCE = 5000
 
 # Read farm training data
+
 EXTRACTED_VALS_FILE = (
-    SAVE_SIZE_FARM_FOLDER + "/01_TrainData/" + model_code[0] + "_ExtractTrain.csv"
+    SAVE_SIZE_FARM_FOLDER + "/01_TrainData/" + model_code[0] + "_ExtractTrain_BufDist_5000.csv"
 )
+
 extr_train = pd.read_csv(EXTRACTED_VALS_FILE)
 extr_train = extr_train.dropna()
+
 
 if len(model_code) != 1:
     for j in range(1, len(model_code)):
@@ -71,7 +74,7 @@ if len(model_code) != 1:
             SAVE_SIZE_FARM_FOLDER
             + "/01_TrainData/"
             + model_code[j]
-            + "_ExtractTrain.csv"
+            + "_ExtractTrain_BufDist_5000.csv"
         )
         extr_train_add = pd.read_csv(EXTRACTED_VALS_FILE)
         extr_train = pd.concat([extr_train, extr_train_add])
@@ -111,10 +114,10 @@ feature_list = list(features.columns)
 features = np.array(features)
 
 # Initialise the Scaler
-scaler_features = StandardScaler().fit(features)
+#scaler_features = StandardScaler().fit(features)
 
 # To scale data
-features = scaler_features.fit_transform(features)
+#features = scaler_features.fit_transform(features)
 
 ## _____________________END SCALE FEATURES _____________________###
 
@@ -122,7 +125,7 @@ features = scaler_features.fit_transform(features)
 # Split the data into training and testing sets
 if SPLIT:
     train_features, test_features, train_labels, test_labels = train_test_split(
-        features, labels, test_size=0.25, random_state=42
+        features, labels, test_size=0.2, random_state=42
     )
 else:
     train_features = features
@@ -135,7 +138,7 @@ score_list = []
 nb_features = ["auto", "sqrt", "log2"]
 for i in nb_features:
     # Instantiate model with 1000 decision trees
-    rf = RandomForestRegressor(n_estimators=500, random_state=10, max_features=i)
+    rf = RandomForestRegressor(n_estimators = 500, random_state = 10, max_features = i)
 
     # Train the model on training data
     rf.fit(train_features, train_labels)
@@ -152,28 +155,59 @@ plt.xticks([1, 2, 3], nb_features, fontsize=18)
 plt.yticks(fontsize=18)
 plt.xlabel("number of features", fontsize=18)
 plt.ylabel("$R^{2}$", fontsize=18)
-plt.savefig(OUTPUT_FOLDER + "/score_vs_nb_features.png", dpi=300, bbox_inches="tight")
+plt.savefig(
+    OUTPUT_FOLDER + "/score_vs_nb_features_" + MODEL_NAME + "_" + RF_TRANSFORM + ".png",
+    dpi=300,
+    bbox_inches="tight",
+)
 plt.close()
 
+print("# -------------------------------------------------------------------")
+print("#           Train and save models (RF and BRT)                       ")
+print("# -------------------------------------------------------------------")
 
 # Instantiate model with 1000 decision trees
-rf = RandomForestRegressor(n_estimators=1000, random_state=42)
+rf = RandomForestRegressor(n_estimators=500, random_state=42)
 
 # Train the model on training data
 rf.fit(train_features, train_labels)
+
+params_brt = {
+    "n_estimators": 500,
+    "max_depth": 4,
+    "min_samples_split": 5,
+    "learning_rate": 0.01,
+    "loss": "squared_error",
+}
+
+reg = GradientBoostingRegressor(**params_brt)
+reg.fit(train_features, train_labels)
 
 
 # save the model to disk
 filename = (
     OUTPUT_FOLDER
     + MODEL_NAME
-    + "_model_BufDist_"
+    + "_modelRF_BufDist_"
     + str(BUFFER_DISTANCE)
     + "_"
     + RF_TRANSFORM
     + ".sav"
 )
 pickle.dump(rf, open(filename, "wb"))
+
+filename = (
+    OUTPUT_FOLDER
+    + MODEL_NAME
+    + "_modelBRT_BufDist_"
+    + str(BUFFER_DISTANCE)
+    + "_"
+    + RF_TRANSFORM
+    + ".sav"
+)
+pickle.dump(reg, open(filename, "wb"))
+
+
 
 print("# -------------------------------------------------------------------")
 print("#                       Importance of covariates                     ")
@@ -185,7 +219,7 @@ df_imp = pd.DataFrame()
 # summarize feature importance
 for i, v in enumerate(importance):
     print("Feature Name : ", feature_list[i])
-    print("Feature: %0d, Score: %.5f" % (i, v))
+    print(f"Feature: {i:0d}, Score: {v:.5f}")
     df_imp[feature_list[i]] = [v]
 
 df_imp.to_csv(
@@ -223,81 +257,60 @@ plt.savefig(
 plt.close()
 
 print("# -------------------------------------------------------------------")
-print("#                       Test validation on 25%                       ")
+print("#                       Test validation on 20%                       ")
 print("# -------------------------------------------------------------------")
 predictions = rf.predict(test_features)
+predictions_brt = reg.predict(test_features)
 
-
+print(scipy.stats.pearsonr(predictions_brt, test_labels))
 print("# -------------------------------------------------------------------")
 print("#                       Internal validation                          ")
 print("# -------------------------------------------------------------------")
 
+# Calculate the absolute errors
+if RF_TRANSFORM == "RobustScalar":
+    y_new = predictions.reshape(-1, 1)
+    y_train = test_labels.reshape(-1, 1)
+    y_new = scaler_label.inverse_transform(y_new)
+    y_train = scaler_label.inverse_transform(y_train)
 
-####### internal validation (predictions where have already data)
-# Use the forest's predict method on the test data
-N_SIM = 1
-accuracy_list = []
-spearman_list = []
-pearson_list = []
-for i in range(N_SIM):
-    # Read farm training data
-    EXTRACTED_VALS_FILE = (
-        SAVE_SIZE_FARM_FOLDER + "/01_TrainData/" + model_code[0] + "_ExtractTrain.csv"
-    )
-    extr_train = pd.read_csv(EXTRACTED_VALS_FILE)
-    extr_train = extr_train.dropna()
+if RF_TRANSFORM == "PowerTransformer":
+    y_new = (predictions * lambda_ + 1) ** (1 / lambda_)
+    y_train = (test_labels * lambda_ + 1) ** (1 / lambda_)
 
-    if len(model_code) != 1:
-        for j in range(1, len(model_code)):
-            EXTRACTED_VALS_FILE = (
-                SAVE_SIZE_FARM_FOLDER
-                + "/01_TrainData/"
-                + model_code[j]
-                + "_ExtractTrain.csv"
-            )
-            extr_train_add = pd.read_csv(EXTRACTED_VALS_FILE)
-            extr_train = pd.concat([extr_train, extr_train_add])
-    extr_train = extr_train.dropna()
-    predictions = rf.predict(test_features)
+if RF_TRANSFORM == "Log":
+    y_new = 10 ** (predictions)
+    y_train = 10 ** (test_labels)
 
-    # Calculate the absolute errors
-    if RF_TRANSFORM == "RobustScalar":
-        y_train = test_labels.reshape(-1, 1)
-        y_new = predictions.reshape(-1, 1)
+errors = abs(y_new - y_train)  # Print out the mean absolute error (mae)
+# print('Mean Absolute Error:', round(np.mean(errors), 2), 'chickens.')
 
-        y_train = scaler_label.inverse_transform(y_train)
-        y_new = scaler_label.inverse_transform(y_new)
+# Calculate mean absolute percentage error (MAPE)
+mape = 100 * (errors / y_train)  # Calculate and display accuracy
+accuracy = 100 - np.mean(mape)
 
-    if RF_TRANSFORM == "PowerTransformer":
-        y_train = (test_labels * lambda_ + 1) ** (1 / lambda_)
-        y_new = (predictions * lambda_ + 1) ** (1 / lambda_)
 
-    if RF_TRANSFORM == "Log":
-        y_train = 10 ** (test_labels)
-        y_new = 10 ** (predictions)
+P_TotalStock = sum(y_train)
+y_new = np.asarray((y_new))
+if P_TotalStock != "NA":
+    adjFactor = P_TotalStock / sum(y_new)
+    y_new = y_new * adjFactor
 
-    errors = abs(y_new - y_train)  # Print out the mean absolute error (mae)
-    # print('Mean Absolute Error:', round(np.mean(errors), 2), 'chickens.')
+pearson = scipy.stats.pearsonr(y_new, y_train)[0]
+spearman = stats.spearmanr(y_new, y_train)[0]
+pearson_log = scipy.stats.pearsonr(predictions, test_labels)[0]
+spearman_log = stats.spearmanr(predictions, test_labels)[0]
 
-    # Calculate mean absolute percentage error (MAPE)
-    mape = 100 * (errors / y_train)  # Calculate and display accuracy
-    accuracy = 100 - np.mean(mape)
-    # print('Accuracy:', round(accuracy, 2), '%.')
-    accuracy_list.append(accuracy)
-
-    P_TotalStock = sum(y_train)
-    y_new = np.asarray((y_new))
-    if P_TotalStock != "NA":
-        adjFactor = P_TotalStock / sum(y_new)
-        y_new = y_new * adjFactor
-
-    pearson_list.append(scipy.stats.pearsonr(y_new, y_train)[0])
-    spearman_list.append(stats.spearmanr(y_new, y_train)[0])
-
-dataframe = pd.DataFrame(
-    {"pearson": pearson_list, "accuracy": accuracy_list, "spearman": spearman_list}
+df = pd.DataFrame(
+    {
+        "Pearson_log": [pearson_log],
+        "Spearman_log": [spearman_log],
+        "Pearson": [pearson],
+        "Spearman": [spearman],
+        "Accuracy": [accuracy],
+    }
 )
-dataframe.to_csv(OUTPUT_FOLDER + "/internal_validation" + "_" + RF_TRANSFORM + ".csv")
+df.to_csv(OUTPUT_FOLDER + "/internal_validation" + "_" + RF_TRANSFORM + ".csv")
 
 
 # plot density histogram
@@ -338,117 +351,87 @@ plt.close()
 
 
 print("# -------------------------------------------------------------------")
-print("#                       External validation                          ")
+print("#                       Predictions on simulated data                ")
 print("# -------------------------------------------------------------------")
-list_code_sim = ["Broiler_THA", "Broiler_BGD", "Broiler_IN.GJ"]
-accuracy_list = []
-spearman_list = []
-pearson_list = []
-rsquared_list = []
+scale_factor_list = [0.5,1,2]
+variance_factor_list = [0.5,1,2]
+NSIM = 100
+SIMULATION_FOLDER = "03_Results/01_SPP/"+ model_code[0]+"/02_Simulations/02_Scenario1/"
 
-for ii, unique_code_sim in enumerate(list_code_sim):
-    # Read farm training data
-    EXTRACTED_VALS_FILE = (
-        SAVE_SIZE_FARM_FOLDER + "/01_TrainData/" + unique_code_sim + "_ExtractTrain.csv"
-    )
+feature_list_bgd = [i+"_BGD" for i in feature_list]
+rename_columns_dict = dict(zip(feature_list_bgd, feature_list))
 
-    extr_train = pd.read_csv(EXTRACTED_VALS_FILE)
-    extr_train = extr_train.dropna()
+# Create combinations of scale and variance factors
+combinations = list(itertools.product(scale_factor_list, variance_factor_list))
 
-    # axis 1 refers to the columns
-    features_ext = extr_train.drop("Stock", axis=1)
-    # Saving feature names for later use
-    feature_list_ext = list(features_ext.columns)
-    # Convert to numpy array
-    features_ext = np.array(features_ext)
-    # To scale data
-    # features_ext = scaler_features.fit_transform(features_ext)
+# loop over the combinations
+for scale_factor, variance_factor in combinations:
+    print(f"Scale Factor: {scale_factor}, Variance Factor: {variance_factor}")
+    for k in range(NSIM) :
+        EXTRACTED_FILE = (
+            SIMULATION_FOLDER
+            + "Simu_fixed_var"
+            + str(variance_factor)
+            + "_sc"
+            + str(scale_factor)
+            + "/ExtractPred_Simufixed_fit_kppm_var"
+            + str(variance_factor)
+            + "_sc"
+            + str(scale_factor)
+            + "_sim_"
+            + str(k + 1)
+            + ".csv"
+        )
 
-    # Use the forest's predict method on the test data
-    predictions_ext = rf.predict(features_ext)
+        extr_sim_spp = pd.read_csv(EXTRACTED_FILE)
+        extr_sim_spp = extr_sim_spp.dropna()
 
-    if RF_TRANSFORM == "RobustScalar":
-        y_predict = predictions.reshape(-1, 1)
-        y_predict = scaler_label.inverse_transform(predictions_ext)
-        y_predict = predictions.ravel()
+        # Rename the columns
+        extr_sim_spp = extr_sim_spp.rename(columns=rename_columns_dict )
 
-    if RF_TRANSFORM == "PowerTransformer":
-        y_predict = (predictions_ext * lambda_ + 1) ** (1 / lambda_)
+        # select features of the RF model
+        features = np.array(extr_sim_spp[feature_list])
 
-    if RF_TRANSFORM == "Log":
-        y_predict = 10 ** (predictions_ext)
+        predictions = rf.predict(features)
 
-    y_test = np.array(extr_train["Stock"])
+        # rescale the predictions
+        if RF_TRANSFORM == "RobustScalar":
+            y_new = predictions.reshape(-1, 1)
+            y_new = scaler_label.inverse_transform(y_new)
 
-    ### readjust number of farms
-    TOTAL_STOCK = sum(y_test)
-    y_predict = np.asarray((y_predict))
-    if TOTAL_STOCK != "NA":
-        adjFactor = TOTAL_STOCK / sum(y_predict)
-        y_predict = y_predict * adjFactor
+        if RF_TRANSFORM == "PowerTransformer":
+            y_new = (predictions * lambda_ + 1) ** (1 / lambda_)
 
-    # plot density histogram
-    fig, ax = plt.subplots(figsize=(6, 5))
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
+        if RF_TRANSFORM == "Log":
+            y_new = 10 ** (predictions)
 
-    density = gaussian_kde(np.log10(y_predict))
-    density2 = gaussian_kde(np.log10(y_test))
-    x_vals = np.linspace(
-        min(np.log10(y_predict)), max(np.log10(y_predict)), 100
-    )  # Specifying the limits of our data
-    x_vals2 = np.linspace(
-        min(np.log10(y_test)), max(np.log10(y_test)), 100
-    )  # Specifying the limits of our data
-    density.covariance_factor = lambda: 0.2  # Smoothing parameter
-    density2.covariance_factor = lambda: 0.2  # Smoothing parameter
-    density._compute_covariance()
-    density2._compute_covariance()
+        # adjust to the total number of chickens
+        P_TotalStock = sum(y_train)
+        y_new = np.asarray((y_new))
+        if P_TotalStock != "NA":
+            adjFactor = P_TotalStock / sum(y_new)
+            y_new = y_new * adjFactor
 
-    plt.plot(x_vals, (density((x_vals))), label="Simulated data")
-    plt.plot(x_vals2, (density2((x_vals2))), label="Training data")
-    plt.xticks(fontsize=13)
-    plt.yticks(fontsize=13)
-    plt.xlabel("log(Farm size)", fontsize=18)
-    plt.ylabel("frequency", fontsize=18)
-    plt.legend(loc="best", fontsize=13)
-    plt.savefig(
-        OUTPUT_FOLDER
-        + "/density function_trained_sim_"
-        + MODEL_NAME
-        + "_Sim_"
-        + unique_code_sim
-        + "_"
-        + RF_TRANSFORM
-        + ".png",
-        bbox_inches="tight",
-        dpi=300,
-    )
-    plt.close()
+        # save the predictions
+        df = pd.DataFrame({"ID": np.array(extr_sim_spp["ID"]), "Stock": [int(ii) for ii in y_new]})
 
-    errors = abs(y_predict - y_test)  # Print out the mean absolute error (mae)
-    # print('Mean Absolute Error:', round(np.mean(errors), 2), 'chickens.')
+        # save the dataframe in a csv file
+        STOCK_SIM_FILE = (
+            SIMULATION_FOLDER
+            + "Simu_fixed_var"
+            + str(variance_factor)
+            + "_sc"
+            + str(scale_factor)
+            + "/Stock_"
+            + str(variance_factor)
+            +"_sc"
+            + str(scale_factor)
+            +"_sim_"
+            + str(k+1)
+            +".csv"
+        )
 
-    # Calculate mean absolute percentage error (MAPE)
-    mape = 100 * (errors / y_test)  # Calculate and display accuracy
-    accuracy = 100 - np.mean(mape)
-    # print('Accuracy:', round(accuracy, 2), '%.')
-    ybar = np.sum(y_predict) / len(y_predict)
-    ssreg = np.sum((y_predict - ybar) ** 2)
-    sstot = np.sum((y_test - ybar) ** 2)
-
-    accuracy_list.append(accuracy)
-    pearson_list.append(scipy.stats.pearsonr(y_predict, y_test)[0])
-    spearman_list.append(stats.spearmanr(y_predict, y_test)[0])
-    rsquared_list.append(ssreg / sstot)
-
-dataframe = pd.DataFrame(
-    {
-        "Country": list_code_sim,
-        "pearson": pearson_list,
-        "accuracy": accuracy_list,
-        "spearman": spearman_list,
-        "rsquared": rsquared_list,
-    }
-)
-dataframe.to_csv(OUTPUT_FOLDER + "/external_validation" + "_" + RF_TRANSFORM + ".csv")
+        df.to_csv(
+            STOCK_SIM_FILE,
+            index=False,
+        )
